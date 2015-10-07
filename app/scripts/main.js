1dd66a870ea4e2935ddceef359da0c3a40e86d4a
Gfäsche <tuelsch@gmail.com>
2015-10-07 18:56:33 +0200

    Fix the jquery selector if there are preceding siblings other than .cd-modal-bg

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -7,15 +7,15 @@ jQuery(document).ready(function($){
 	//trigger the animation - open modal window
 	$('[data-type="modal-trigger"]').on('click', function(){
 		var actionBtn = $(this),
-			scaleValue = retrieveScale(actionBtn.next('.cd-modal-bg'));
+			scaleValue = retrieveScale(actionBtn.nextAll('.cd-modal-bg'));
 		
 		actionBtn.addClass('to-circle');
-		actionBtn.next('.cd-modal-bg').addClass('is-visible').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', function(){
-			animateLayer(actionBtn.next('.cd-modal-bg'), scaleValue, true);
+		actionBtn.nextAll('.cd-modal-bg').addClass('is-visible').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', function(){
+			animateLayer(actionBtn.nextAll('.cd-modal-bg'), scaleValue, true);
 		});
 
 		//if browser doesn't support transitions...
-		if(actionBtn.parents('.no-csstransitions').length > 0 ) animateLayer(actionBtn.next('.cd-modal-bg'), scaleValue, true);
+		if(actionBtn.parents('.no-csstransitions').length > 0 ) animateLayer(actionBtn.nextAll('.cd-modal-bg'), scaleValue, true);
 	});
 
 	//trigger the animation - close modal window
